9fb1d338a6c446d1dfbe075b15664cafa0b338da
James West <james@jameswest.co.nz>
2014-12-01 17:14:53 +1300

    First check if symlinks already exist

--- update.sh
+++ update.sh
@@ -5,6 +5,12 @@ git reset --hard origin/master
 git submodule update --init --recursive
 chmod -R 605 .
 rm -rf skel/public/wp.symlink
-ln -s ${DIR}/wp skel/public/wp
-rm -rf skel/public/sites/themes/twentyfourteen.symlink
-ln -s ${DIR}/wp/wp-content/themes/twentyfourteen skel/public/site/themes/twentyfourteen
+if [ ! -h skel/public/wp ] 
+then
+	ln -s ${DIR}/wp skel/public/wp
+fi
+rm -rf skel/public/site/themes/twentyfourteen.symlink
+if [ ! -h skel/public/site/themes/twentyfourteen ] 
+then
+	ln -s ${DIR}/wp/wp-content/themes/twentyfourteen skel/public/site/themes/twentyfourteen
+fi
