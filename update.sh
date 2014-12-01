d97e3c937acab8be96cc106928e021f16fba30fb
James West <james@jameswest.co.nz>
2014-12-01 17:09:50 +1300

    More path fixes

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ git reset --hard origin/master
 git submodule update --init --recursive
 chmod -R 605 .
 rm -rf skel/public/wp.symlink
-ln -s ./wp skel/public/wp
+ln -s ${DIR}/wp skel/public/wp
 rm -rf skel/public/sites/themes/twentyfourteen.symlink
-ln -s ./wp/wp-content/themes/twentyfourteen skel/site/themes/twentyfourteen
+ln -s ${DIR}/wp/wp-content/themes/twentyfourteen skel/public/site/themes/twentyfourteen
