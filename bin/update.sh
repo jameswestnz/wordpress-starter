fea5d9dd04548a4021afcc4d2b86713e026a3771
James West <james@jameswest.co.nz>
2014-12-01 19:21:34 +1300

    Commented update script, fixed permissions

--- bin/update.sh
+++ bin/update.sh
@@ -1,14 +1,28 @@
+# move to the script’s directory
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
+
+# reset to latest remote
 git fetch --all
 git reset --hard origin/master
+
+# update all submodules
 git submodule update --init --recursive
-chmod -R 605 .
+
+# fix all permissions
+chmod -R 600 .
+chmod -R 644 skel
+chmod -R 655 wp wp-config.php
+
+# remove symlink placeholders and setup symlinks
+## wordpress
 rm -rf skel/public/wp.symlink
 if [ ! -h skel/public/wp ] 
 then
 	ln -s ${DIR}/wp skel/public/wp
 fi
+
+## twentyfourteen theme
 rm -rf skel/public/site/themes/twentyfourteen.symlink
 if [ ! -h skel/public/site/themes/twentyfourteen ] 
 then
