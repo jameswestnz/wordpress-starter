eca350f16612aadd416613059ef2df38b5978fc4
James West <james@jameswest.co.nz>
2014-12-01 17:05:50 +1300

    Fixed wp path

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git fetch --all
 git reset --hard origin/master
 git submodule update --init --recursive
 chmod -R 605 .
-rm -rf skel/wp.symlink
-ln -s ${DIR}/wp skel/wp
+rm -rf skel/public/wp.symlink
+ln -s ./wp skel/public/wp
 rm -rf skel/public/sites/themes/twentyfourteen.symlink
-ln -s ${DIR}/wp/wp-content/themes/twentyfourteen skel/site/themes/twentyfourteen
+ln -s ./wp/wp-content/themes/twentyfourteen skel/site/themes/twentyfourteen
