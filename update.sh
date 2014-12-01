b357427fb40cb97daad8bc2441dc3465e77bee13
James West <james@jameswest.co.nz>
2014-12-01 17:02:26 +1300

    Fixed quote issues

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ git reset --hard origin/master
 git submodule update --init --recursive
 chmod -R 605 .
 rm -rf skel/wp.symlink
-ln -s “${DIR}/wp” skel/wp
+ln -s ${DIR}/wp skel/wp
 rm -rf skel/public/sites/themes/twentyfourteen.symlink
-ln -s “${DIR}/wp/wp-content/themes/twentyfourteen” skel/site/themes/twentyfourteen
+ln -s ${DIR}/wp/wp-content/themes/twentyfourteen skel/site/themes/twentyfourteen
