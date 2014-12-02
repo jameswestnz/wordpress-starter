390567bed2a029ae9011a1d212257bb14d7c5755
James West <james@jameswest.co.nz>
2014-12-02 16:05:05 +1300

    Removed DB settings in skel wp-config.php

--- skel/wp-config-sample.php
+++ skel/wp-config-sample.php
@@ -12,12 +12,6 @@ define('DB_PASSWORD', '%db_password%');
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
